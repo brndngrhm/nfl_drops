f9502fed0b9c13b404e884963f843d87651feff2
brndngrhm <brendanvgraham@gmail.com>
2016-01-08 12:22:57 -0500

    adds horizontal average line to league-wide plots

--- drops.R
+++ drops.R
@@ -1,4 +1,4 @@
-#load necessary libraries
+#load necessary libraries----
 library(dplyr)
 library(ggplot2)
 library(ggthemes)
@@ -8,7 +8,7 @@ library(rvest)
 #
 #
 
-#scrape data using rvest package
+#scrape data using rvest package----
 url.2015 <- "http://www.sportingcharts.com/nfl/stats/drops/2015/"
 url.2014 <- "http://www.sportingcharts.com/nfl/stats/drops/2014/"
 url.2013 <- "http://www.sportingcharts.com/nfl/stats/drops/2013/"
@@ -17,13 +17,15 @@ url.2011 <- "http://www.sportingcharts.com/nfl/stats/drops/2011/"
 url.2010 <- "http://www.sportingcharts.com/nfl/stats/drops/2010/"
 url.2009 <- "http://www.sportingcharts.com/nfl/stats/drops/2009/"
 
-drops.2015 <- as.data.frame(url.2015 %>% html() %>% html_nodes(xpath = '//*[@id="ff65cc4c2e374d8f806e203f861c0e3d"]') %>% html_table())
-drops.2014 <- as.data.frame(url.2014 %>% html() %>% html_nodes(xpath = '//*[@id="e5a9515a9bd14ea2ae68dbaff926d32f"]') %>% html_table())
-drops.2013 <- as.data.frame(url.2013 %>% html() %>% html_nodes(xpath = '//*[@id="acad99da07164b65bbb80996d24dc00c"]') %>% html_table())
-drops.2012 <- as.data.frame(url.2012 %>% html() %>% html_nodes(xpath = '//*[@id="8fae62b0b21a410fadfd97e32cbc7e54"]') %>% html_table())
-drops.2011 <- as.data.frame(url.2011 %>% html() %>% html_nodes(xpath = '//*[@id="eb32d60dc0164ad8b97e1290e75a1439"]') %>% html_table())
-drops.2010 <- as.data.frame(url.2010 %>% html() %>% html_nodes(xpath = '//*[@id="a9a5c37b083f4eaf9b2a041eaee0fe42"]') %>% html_table())
-drops.2009 <- as.data.frame(url.2009 %>% html() %>% html_nodes(xpath = '//*[@id="f5babb149b6c4719b23215e78c9405a7"]') %>% html_table())
+drops.2015 <- as.data.frame(url.2015 %>% html() %>% html_nodes(xpath = '//*[@id="ccf80cdbbdfa4d39b6df47dc7cff2fc5"]') %>% html_table())
+drops.2014 <- as.data.frame(url.2014 %>% html() %>% html_nodes(xpath = '//*[@id="9b21e1ac7c3e4b7f9d660adc49d12b34"]') %>% html_table())
+drops.2013 <- as.data.frame(url.2013 %>% html() %>% html_nodes(xpath = '//*[@id="6ac7439236cf42efbb4d2c798e172abb"]') %>% html_table())
+drops.2012 <- as.data.frame(url.2012 %>% html() %>% html_nodes(xpath = '//*[@id="4d2fbd64923b49458959bc235809cfc1"]') %>% html_table())
+drops.2011 <- as.data.frame(url.2011 %>% html() %>% html_nodes(xpath = '//*[@id="919bbc4d6342462dac040492062f3c54"]') %>% html_table())
+drops.2010 <- as.data.frame(url.2010 %>% html() %>% html_nodes(xpath = '//*[@id="cc4d7cff89c8401aadcd2299a98161cd"]') %>% html_table())
+drops.2009 <- as.data.frame(url.2009 %>% html() %>% html_nodes(xpath = '//*[@id="8e4b04f8252a4d99899526d1761789ad"]') %>% html_table())
+
+#Formats and combines data----
 
 #adds year column to each dataframe
 drops.2015$year <- "2015"
@@ -34,7 +36,7 @@ drops.2011$year <- "2011"
 drops.2010$year <- "2010"
 drops.2009$year <- "2009"
 
-#combines and formats data frames 
+#combines and formats data frames
 drops <- rbind(drops.2009, drops.2010, drops.2011, drops.2012, drops.2013, drops.2014, drops.2015)
 names(drops) <- tolower(names(drops))
 names(drops)[8] <- "comp.rate"
@@ -62,6 +64,8 @@ str(drops)
 #
 #
 
+#league-wide plots----
+
 #create subsets for plotting
 drops.2015 <- drops %>% filter(year == 2015) %>% select(team, drops, flag) %>% group_by(team, flag) %>% summarise(drops = sum(drops))
 drop.rate.2015 <- drops %>% filter(year == 2015) %>% select(team, drop.rate, flag) %>% group_by(team, flag) %>% summarise(mean.drop.rate =mean(drop.rate))
@@ -71,27 +75,26 @@ position.rate <- drops %>% filter(year == 2015) %>% select(pos, drop.rate, flag)
 #various league-wide plots
 (team.drops <- ggplot(drops.2015, aes(x=reorder(team, -drops), y=drops, fill = flag))
 + geom_bar(stat = "identity", alpha = .6) + scale_fill_manual(values = c("gray37", "darkgreen")) + labs(x="\nTeam", y="Drops\n", title="Drops per Team (2015 Season)\n")
-+ guides(fill=FALSE))
++ guides(fill=FALSE) + geom_hline(aes(yintercept=mean(drops), alpha=.6)))
 
 (team.drop.rate <- ggplot(drop.rate.2015, aes(x=reorder(team, -mean.drop.rate), y=mean.drop.rate, fill = flag))
 + geom_bar(stat = "identity", alpha = .6) + scale_fill_manual(values = c("gray37", "darkgreen")) + labs(x="\nTeam", y="Mean Drop Rate\n", title="Mean Drop Rate per Team (2015 Season)\n")
-+ guides(fill=FALSE))
++ guides(fill=FALSE)+ geom_hline(aes(yintercept=mean(mean.drop.rate), alpha=.6)))
 
 (potition.drops <- ggplot(subset(position, drops>0), aes(x=reorder(pos, -drops), y=drops, fill = pos))
 + geom_bar(stat = "identity", alpha = .6) + scale_fill_manual(values = c("gray37", "gray37", "gray37")) + labs(x="\nPosition", y="Drops\n", title="Drops per Position (2015 Season)\n")
-+ guides(fill=FALSE))
++ guides(fill=FALSE)+ geom_hline(aes(yintercept=mean(drops), alpha=.6)))
 
 (potition.drop.rate <- ggplot(subset(position.rate, drop.rate>0), aes(x=reorder(pos, -drop.rate), y=drop.rate, fill = pos))
 + geom_bar(stat = "identity", alpha = .6) + scale_fill_manual(values = c("gray37", "gray37", "gray37")) + labs(x="\nPosition", y="League Wide Mean Drop Rate\n", title="Mean Drop Rate per Position (2015 Season)\n")
-+ guides(fill=FALSE))
++ guides(fill=FALSE)+ geom_hline(aes(yintercept=mean(drop.rate), alpha=.6)))
 
 (potition.team.drop.rate <- ggplot(subset(position.rate, drop.rate>0), aes(x=pos, y=drop.rate, fill = flag))
 + geom_bar(stat = "identity", position = "dodge", alpha = .6) + scale_fill_manual(values = c("gray37", "darkgreen"))
 + labs(x = "", y = "Mean Drop Rate\n", title = "Drop Rates - NFL Avg. vs Eagles Avg.\n(2015 Season)\n")
 + guides(fill=FALSE))
-#
-#
-#
+
+#PHL plots----
 
 #creating subsets of drops for PHI plotting 
 eagles <- drops %>% filter(team == "PHI")
@@ -129,39 +132,6 @@ eagles.scatter <- eagles %>% select(pos, drops, targets, year)
 (targets <- ggplot(eagles, aes(x=year, y=targets, fill=year)) +
   geom_bar(stat = "identity", alpha = .6) + scale_fill_manual(values = c("darkgreen", "darkgreen", "darkgreen", "darkgreen", "darkgreen", "darkgreen", "darkgreen")) + 
   labs(y="Targets\n", x="", title = "PHI Targets per Year\n") + guides(fill=FALSE))
-
-
-#some scatterplots
-(scatter.2009 <- ggplot(subset(drops, team == "PHI" & year == 2009), aes(x=targets, y=drops, color=pos))+ 
-  geom_point(size = 2) + geom_jitter() + 
-  labs(y="Drops\n", x="", title = "2009 PHI Drops per Position\n") + guides(fill=FALSE) + labs(fill=''))
-
-(scatter.2010 <- ggplot(subset(drops, team == "PHI" & year == 2010), aes(x=targets, y=drops, color=pos))+ 
-  geom_point(size = 2) + geom_jitter() + 
-  labs(y="Drops\n", x="", title = "2010 PHI Drops per Position\n") + guides(fill=FALSE) + labs(fill=''))
-
-(scatter.2011 <- ggplot(subset(drops, team == "PHI" & year == 2011), aes(x=targets, y=drops, color=pos))+ 
-  geom_point(size = 2) + geom_jitter() + 
-  labs(y="Drops\n", x="", title = "2011 PHI Drops per Position\n") + guides(fill=FALSE) + labs(fill=''))
-
-(scatter.2012 <- ggplot(subset(drops, team == "PHI" & year == 2012), aes(x=targets, y=drops, color=pos))+ 
-  geom_point(size = 2) + geom_jitter() + 
-  labs(y="Drops\n", x="", title = "2012 PHI Drops per Position\n") + guides(fill=FALSE) + labs(fill=''))
-
-(scatter.2013 <- ggplot(subset(drops, team == "PHI" & year == 2013), aes(x=targets, y=drops, color=pos))+ 
-  geom_point(size = 2) + geom_jitter() + 
-  labs(y="Drops\n", x="", title = "2013 PHI Drops per Position\n") + guides(fill=FALSE) + labs(fill=''))
-
-(scatter.2014 <- ggplot(subset(drops, team == "PHI" & year == 2014), aes(x=targets, y=drops, color=pos))+ 
-  geom_point(size = 2) + geom_jitter() + 
-  labs(y="Drops\n", x="", title = "2014 PHI Drops per Position\n") + guides(fill=FALSE) + labs(fill=''))
-
-(scatter.2015 <- ggplot(subset(drops, team == "PHI" & year == 2015), aes(x=targets, y=drops, color=pos))+ 
-  geom_point(size = 2) + geom_jitter() + 
-  labs(y="Drops\n", x="", title = "2015 PHI Drops per Position\n") + guides(fill=FALSE) + labs(fill=''))
-#
-#
-#
 
 #corellation stats
 cor(drops$targets, drops$drops)
